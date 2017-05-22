4eca8ea9db583a0d533bbafe2a4d6b0bad144238
Francisco Vergara <javiervergara2004@gmail.com>
2017-05-22 01:42:08 -0400

    FIX: Se elimina carpeta cache en vendors

--- vendor/cache/ruby/2.4.0/gems/angular-ui-bootstrap-rails-0.11.0/lib/angular-ui-bootstrap-rails/version.rb
+++
@@ -1,7 +0,0 @@
-module AngularUI
-  module Bootstrap
-    module Rails
-      VERSION = "0.11.0"
-    end
-  end
-end
--- vendor/cache/ruby/2.4.0/gems/angular-ui-bootstrap-rails-2.4.0/lib/angular-ui-bootstrap-rails/version.rb
+++
@@ -1,7 +0,0 @@
-module AngularUI
-  module Bootstrap
-    module Rails
-      VERSION = "2.4.0"
-    end
-  end
-end
